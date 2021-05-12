400a88c4003327536835f3bc110d8431d792f8a9
tanishgupta-git <tanishgupta8181@gmail.com>
2021-05-12 18:33:05 +0530

    added a chatscreen component

--- components/Chat.js
+++ components/Chat.js
@@ -4,16 +4,20 @@ import { useCollection } from "react-firebase-hooks/firestore";
 import styled from "styled-components";
 import { auth,db } from "../firebase";
 import getRecipientEmail from "../utils/getRecipientemail";
+import {useRouter } from 'next/router';
 
 const Chat = ({id,users}) => {
      const router = useRouter();
     const [user] = useAuthState(auth);
     const [recipientSnapshot] = useCollection(db.collection('users').where('email','==',getRecipientEmail(users,user))) 
+    const enterChat = () => {
+            router.push(`/chat/${id}`)
+    }
     const recipient = recipientSnapshot?.docs?.[0]?.data();
     const recipientEmail = getRecipientEmail(users,user);  
  
     return (  
-        <Container>
+        <Container onClick={enterChat}>
          {
              recipient ? (
              <UserAvatar src={recipient?.photoURL}/> 
---
+++ pages/chat/[id].js
@@ -0,0 +1,35 @@
+import Head from "next/head";
+import styled from "styled-components";
+import ChatScreen from "../../components/ChatScreen";
+import Sidebar from "../../components/Sidebar";
+
+const Chat = () => {
+    return (  
+        <Container>
+            <Head>
+                <title>Chat</title>
+            </Head>
+            <Sidebar />
+            <ChatContainer>
+               <ChatScreen />
+            </ChatContainer>
+        </Container>
+    );
+}
+ 
+export default Chat;
+
+const Container = styled.div`
+display:flex;`;
+
+const ChatContainer = styled.div`
+flex:1;
+overflow:scroll;
+height:100vh;
+
+::-webkit-scrollbar {
+    display:none;
+}
+-ms-overflow-style:none;
+scrollbar-width:none;
+`;
